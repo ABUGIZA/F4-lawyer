4a9de8164c7fc111b8726dd2469455cf42e27a73
ABUGIZA <100775763+ABUGIZA@users.noreply.github.com>
2024-03-29 21:31:20 +0300

    Add files via upload

---
+++ F4-lawyer/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games {'gta5'}
+
+description 'F4-Lawyer'
+version '1.0'
+
+client_scripts {
+'client/client.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+}
